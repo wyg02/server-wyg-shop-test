a9831e1158e4e7be544ab1e8f9468921184e1f7e
魏阳光 <2127494056@qq.com>
2022-12-08 18:27:00 +0800

    1/初始化项目的提交

---
+++ server-wyg-order/src/main/java/com/bwie/ServerWygOrderApplication.java
@@ -0,0 +1,13 @@
+package com.bwie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerWygOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerWygOrderApplication.class, args);
+    }
+
+}
---
+++ server-wyg-product/src/main/java/com/bwie/ServerWygProductApplication.java
@@ -0,0 +1,13 @@
+package com.bwie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerWygProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerWygProductApplication.class, args);
+    }
+
+}
