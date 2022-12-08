c6d125cdc87cdeb3e4264b0ec2a3832a65a77a98
魏阳光 <2127494056@qq.com>
2022-12-08 19:21:38 +0800

    1/初始化项目的提交

---
+++ server-wyg-product/src/main/java/com/bwie/controller/a.java
@@ -0,0 +1,11 @@
+package com.bwie.controller;
+
+/**
+ * @Author Sun
+ * @Version 1.0
+ * @description: TODO
+ * @Date 2022/12/8 18:40:04
+ */
+public class a {
+
+}
---
+++ server-wyg-product/src/main/java/com/bwie/controller/b.java
@@ -0,0 +1,10 @@
+package com.bwie.controller;
+
+/**
+ * @Author Sun
+ * @Version 1.0
+ * @description: TODO
+ * @Date 2022/12/8 18:40:26
+ */
+public interface b {
+}
